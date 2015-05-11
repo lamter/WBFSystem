00a0c96a2342498aa6aa1860214dee936e3ccda9
lamter <lamter.fu@gmail.com>
2015-05-11 11:42:28 +0800

    gevent 并发的配置和修改
     - add : 添加 gevent==1.0.1 到依赖库文件
     - change : 将启动脚本改为 gevent 并发

--- src/sh/start_uwsgi.sh
+++ src/sh/start_uwsgi.sh
@@ -2,5 +2,6 @@
 # start_server.sh
 
 cd ../www
-uwsgi --http :8080 -p 1 -w main -T --pyargv "--dbhost localhost --dbp 8911 --db 1 " -d ./log/wbfs.log
+# uwsgi --http :8080 -p 1 -w main -T --pyargv "--dbhost localhost --dbp 8911 --db 1 " -d ./log/wbfs.log
+uwsgi --gevent 100 --gevent-monkey-patch --http :8080 -M  -p 1 --wsgi-file myapp.py --pyargv "--dbhost localhost --dbp 8911 --db 1 " -d ./log/wbfs.log
 ps -ef|grep uwsgi

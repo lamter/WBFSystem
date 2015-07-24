b60bac9b075bee03704c23c82f1573164c404ad1
lamter <lamter.fu@gmail.com>
2015-07-24 17:09:29 +0800

    - add : 添加并发业务

--- src/www/pool.py
+++ src/www/pool.py
@@ -12,16 +12,11 @@ from gevent.pool import Pool
 from app import settings
 import loop
 
-
-
-
-
-
-
-
+from app.models.task import BaseTask
 
 ''' 生成并发池 '''
 pool = Pool(settings.ASYNC)
+
 
 
 def get():
@@ -29,8 +24,12 @@ def get():
     :return:
     '''
 
+    ''' 添加一个测试任务 '''
+    task = BaseTask()
+    pool.start(task)
+
     ''' 主业务循环 '''
-    pool.join(loop.loop)
+    pool.spawn(loop.loop)
 
     web.ctx.pool = pool
     return pool

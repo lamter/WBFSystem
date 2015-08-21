5eee7dfb2de176959db2550919787aee36ed1241
lamter <lamter.fu@gmail.com>
2015-08-21 18:12:00 +0800

    - change : 添加忽略文件

--- src/www/conf_debug.py
+++ src/www/conf_debug.py
@@ -16,6 +16,6 @@ settings.DEBUG = True
 
 ''' 在调试中不需要输出到日志，直接指向 '''
 settings.loglevel = logging.INFO
-settings.IS_LOG_TO_FILE = False
+settings.IS_LOG_TO_FILE = True
 # settings.log.close()
 # settings.log = 'default'

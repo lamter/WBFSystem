4d47bba38a6b55642c1c3bc4890079943810fac8
lamter <lamter.fu@gmail.com>
2015-09-15 21:46:32 +0800

    - fix : 本地空log的支持

--- src/www/static/js/execpython.js
+++ src/www/static/js/execpython.js
@@ -1,49 +1,27 @@
 /*
- * python 实时刷新log - v1.1 - 9/10/2015
- * cordial
+ * 提交要执行的 python 代码 - v1.1 - 9/10/2015
+ * Shawn
  */
 
-var logArray = []
-var requestData = {};
-var log = {
-    refreshLog: function(docId, url, reqData, reqMethod, logNum){
-        setInterval(this.showLog, 2000, docId, url, reqData, reqMethod, logNum)
-    },
 
-    showLog: function(docId, url, reqData, reqMethod, logNum){
-        // 循环请求log数据
-        // alert(url)
+var execPython = {
+    run: function(url, code){
+        // 要传递的参数
+        requestData = {};
+        requestData.code = code
+
         jQuery.ajax({
-            type: reqMethod,
+            type: 'POST',
             url: url,
             timeout: 1000,
             dataType: 'json',
             data: jQuery.param(requestData, true),
+
             error: function(errorData, status, error){
                 //alert(status)
-                logArray.push('requestError:' + status)
+                alert('requestError:' + status)
             },
-            success: function(data){
-                if(data.log){
-                    // data 数据格式 {tag 1, type 2001, log [logStr, ...]}
-                    requestData.tag = data.tag
-                    for(var logStr in data.log){
-                        if(logArray.length > logNum){
-                            // 如果大于log数量, 先踢出一个元素
-                            logArray.shift()
-                        }
-                        logArray.push(data.log[logStr])
-                    }
-                    var docShow = jQuery(docId);
-                    docShow.html();// 清空数据, 后他们刷新数据
-                    tmpHtml = ''
-                    for(var logStr in logArray){
-                        tmpHtml = tmpHtml + logArray[logStr] + '<br>';
-                    }
-                    docShow.html(tmpHtml)
-                }
-                refreshLog(docId, url, reqData, logNum)
-            }
+
         });
     }
 
